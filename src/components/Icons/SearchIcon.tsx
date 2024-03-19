28cf3ad24d9eaa19721c183e5155d050413e3fdf
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2024-03-18 19:55:37 -0500

    searchbar updated

--- src/components/Icons/SearchIcon.tsx
+++ src/components/Icons/SearchIcon.tsx
@@ -3,23 +3,21 @@ const SearchIcon = ({ size = 24, color = "#98A1A4" }: IconProps) => {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 24 24"
+      viewBox="0 0 17 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle
-        cx="11"
-        cy="11"
-        r="8"
+        cx="7.86458"
+        cy="7.33333"
+        r="5.33333"
         stroke={color}
-        stroke-width="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M16.5 16.958L21.5 21.958"
+        d="M11.5312 11.3056L14.8646 14.639"
         stroke={color}
-        stroke-width="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
